05e4a1b166c7de4c075a92e497495aa4e71e0297
omartrese <omarlayachi13@gmail.com>
2024-07-22 17:42:36 +0200

    Chapter 2 added

--- Assets/Scripts/chatperTwoBehaviour.cs
+++ Assets/Scripts/chatperTwoBehaviour.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class chapterOneBehaviour : MonoBehaviour
+public class chatperTwoBehaviour : MonoBehaviour
 {
     [SerializeField]
     private dialogueScript dialogueScript;
@@ -15,14 +15,15 @@ public class chapterOneBehaviour : MonoBehaviour
 
     IEnumerator manageScene()
     {
-        yield return new WaitForSeconds(10); // The title fade-in-out animation has 8s duration
+        yield return new WaitForSeconds(2); // The title fade-in-out animation has 8s duration
 
         print(dialogueScript.isActive);
         dialogueScript.gameObject.SetActive(true);
         dialogueScript.StartDialogue();
         yield return new WaitUntil(() => !dialogueScript.isActive);
 
-        SceneManager.LoadScene(2); //Load chapter 2
+        //SceneManager.LoadScene(2); //Load chapter 2
+        //SceneManager.LoadScene(2); //Load chapter 2
 
     }
 }
