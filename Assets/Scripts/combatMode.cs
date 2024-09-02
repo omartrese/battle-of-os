582c6eaadb412eeaaae97f36c74965742904cab9
omartrese <omarlayachi13@gmail.com>
2024-09-02 17:56:06 +0200

    a little more HP for the player

--- Assets/Scripts/combatMode.cs
+++ Assets/Scripts/combatMode.cs
@@ -5,17 +5,18 @@ using UnityEngine.UI;
 
 public class combatMode : MonoBehaviour
 {
-    private const float HP_MAX = 80, MAX_ATTACK= 100, MAX_DEFENSE = 100;
+    private const float HP_MAX = 100, MAX_ATTACK= 100, MAX_DEFENSE = 100;
 
 
 
+    [Range(0, 100)]
     [SerializeField]
     private float currHP = 80;
 
     [Space(height: 20)]
-
     [SerializeField]
     private Image HPBar;
+
 
     void Start()
     {
